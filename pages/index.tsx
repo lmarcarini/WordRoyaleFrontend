20865cd213c9897c9483cfa40b1f3cba3d067404
lmarcarini <leonardo.marcarini@yahoo.com.br>
2022-04-07 16:59:13 -0300

    change from http to ws

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ import WinnerScreen from "../components/WinnerScreen";
 
 const apiUrl =
   process.env.NODE_ENV === "production"
-    ? "https://word-royale-backend.herokuapp.com/"
+    ? "ws://word-royale-backend.herokuapp.com/"
     : "ws://localhost:8080";
 
 const socket = io(apiUrl);
